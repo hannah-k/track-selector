66331371307f3c468121995771c415c8e1668847
Hannah Kinne <hnkinne@gmail.com>
2017-04-28 12:28:51 -0700

    create js algorithm to determine which epicodus track you should select.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,29 @@
 $(document).ready(function(){
-  $("tracj-quiz").submit(function(event){
+  $("#track-quiz").submit(function(event){
     event.preventDefault();
 
     // collect the input
+    var name = $("#name").val();
+    var question1 = parseInt($("input:radio[name=question1]:checked").val());
+    var question2 = parseInt($("input:radio[name=question2]:checked").val());
+    var question3 = parseInt($("input:radio[name=question3]:checked").val());
+    var question4 = parseInt($("input:radio[name=question4]:checked").val());
 
     // declare new variables to equal the corresponding modal
 
-    // show match modal
+    var track = question1 + question2 + question3;
 
+    if (track <= 5) {
+      track = "CSS/Design!"
+    } else if (track >= 10 && track <= 21) {
+      track = "PHP and Drupal"
+    } else if (track >= 22) {
+      track = "Ruby"
+    }
+
+    // show match modal
+    $("results").show();
+    $(".user-name").text(name);
+    $(".which-one").text(track);
   })
 })
