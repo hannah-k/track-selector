a77bb13a75e5f991c6592e52f1ee3dcf0e8e89bf
Hannah Kinne <hnkinne@gmail.com>
2017-04-28 11:48:45 -0700

    add 2 additional questions.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("tracj-quiz").submit(function(event){
+    event.preventDefault();
+
+    // collect the input
+
+    // declare new variables to equal the corresponding modal
+
+    // show match modal
+
+  })
+})
